97e007f9b958fc9247fb38b2516835e4618c84a4
pattama422 <pattama@camt.info>
2025-12-19 09:51:52 +0700

    listile

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -8,13 +8,29 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+
+  List mydata = ['apple', 'banana','papaya'];
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Center(
-          child: Text("Contact Page"),
-        )
+    ListTile(
+      onTap: () {},
+      leading: Icon(Icons.abc_sharp),
+      title: Text(mydata[0]),
+      subtitle: Text("subtitle"),
+      trailing: FlutterLogo(),
+    ),
+      ListTile(
+      onTap: () {},
+      leading: FlutterLogo(),
+      title: Text(mydata[1]),
+    ),
+      ListTile(
+      onTap: () {},
+      leading: FlutterLogo(),
+      title: Text(mydata[2]),
+    ),
       ],
     );
   }
