49a0366005a242591546223f022597dd3fb7b1a5
pattama422 <pattama@camt.info>
2025-12-26 10:03:07 +0700

    add image function and add local json data

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,24 +15,24 @@ class _HomePageState extends State<HomePage> {
       padding: const EdgeInsets.all(24.0),
       child: ListView(
         children: [
-          MyBox("What is a computer?", "A computer is a machine that can be programmed to automatically."),
+          MyBox("What is a computer?", "A computer is a machine that can be programmed to automatically.","https://cdn.pixabay.com/photo/2015/05/15/02/07/computer-767781_1280.jpg"),
           SizedBox(height: 24),
-          MyBox("What is a flutter?", "Flutter is an open-source UI software development kit created by Google."),
+          MyBox("What is a flutter?", "Flutter is an open-source UI software development kit created by Google.","https://cdn.pixabay.com/photo/2015/05/15/02/07/computer-767781_1280.jpg"),
           SizedBox(height: 24),
-          MyBox("What is a Dart?", "Dart is a general-purpose programming language developed by Google."),
+          MyBox("What is a Dart?", "Dart is a general-purpose programming language developed by Google.","https://cdn.pixabay.com/photo/2015/05/15/02/07/computer-767781_1280.jpg"),
         ],
       ),
     );
   }
 
-  Widget MyBox(String title, String subtitle) {
+  Widget MyBox(String title, String subtitle, String image_link) {
     return Container(
       padding: EdgeInsets.all(24),
       height: 185,
       decoration: BoxDecoration(
         color: Colors.deepPurple[200],
         borderRadius: BorderRadius.circular(24),
-        image: DecorationImage(image:   NetworkImage("https://cdn.pixabay.com/photo/2015/05/15/02/07/computer-767781_1280.jpg"),
+        image: DecorationImage(image:   NetworkImage(image_link),
         fit: BoxFit.cover, 
         colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.7), BlendMode.darken)),
         
