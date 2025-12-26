60be9db85a8ea122beddf517c587135fa44e37e3
pattama422 <pattama@camt.info>
2025-12-26 10:34:53 +0700

    read data from data.json

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:firstapp/pages/details.dart';
 import 'package:flutter/material.dart';
 
@@ -13,26 +14,27 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(24.0),
-      child: ListView(
-        children: [
-          MyBox("What is a computer?", "A computer is a machine that can be programmed to automatically.","https://cdn.pixabay.com/photo/2015/05/15/02/07/computer-767781_1280.jpg"),
-          SizedBox(height: 24),
-          MyBox("What is a flutter?", "Flutter is an open-source UI software development kit created by Google.","https://cdn.pixabay.com/photo/2015/05/15/02/07/computer-767781_1280.jpg"),
-          SizedBox(height: 24),
-          MyBox("What is a Dart?", "Dart is a general-purpose programming language developed by Google.","https://cdn.pixabay.com/photo/2015/05/15/02/07/computer-767781_1280.jpg"),
-        ],
-      ),
+      child: FutureBuilder(builder: (context, snapshot){
+        var data = json.decode(snapshot.data.toString());
+        return ListView.builder(itemBuilder: (BuildContext context, int index){
+          return MyBox(data[index]["title"], data[index]["subtitle"], data[index]["image_url"]);
+
+        },
+        itemCount: data.length);
+      },
+      future: DefaultAssetBundle.of(context).loadString("data.json")),
     );
   }
 
-  Widget MyBox(String title, String subtitle, String image_link) {
+  Widget MyBox(String title, String subtitle, String image_url2) {
     return Container(
       padding: EdgeInsets.all(24),
+      margin: EdgeInsets.fromLTRB(0,8,0,8),
       height: 185,
       decoration: BoxDecoration(
         color: Colors.deepPurple[200],
         borderRadius: BorderRadius.circular(24),
-        image: DecorationImage(image:   NetworkImage(image_link),
+        image: DecorationImage(image:   NetworkImage(image_url2),
         fit: BoxFit.cover, 
         colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.7), BlendMode.darken)),
         
@@ -54,7 +56,8 @@ class _HomePageState extends State<HomePage> {
            SizedBox(height: 24,),
            TextButton(onPressed: (){
             Navigator.push(context, MaterialPageRoute(builder: (context)=> DeatilsPage()));
-           }, child: Text("read more", style: TextStyle(color: const Color.fromARGB(255, 206, 160, 175),)))
+           }, child: Text("read more", style: TextStyle(color: const Color.fromARGB(255, 206, 160, 175),))),
+
         ],
       ),
     );
